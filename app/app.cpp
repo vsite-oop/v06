98a9e2439c3f56ac54ef3bcc6373cee0da32ce56
Giancarlo <giancarlo.bedek@hotmail.com>
2022-11-21 20:18:55 +0100

    Finished the assignment

--- app/app.cpp
+++ app/app.cpp
@@ -1,7 +1,75 @@
 #include "app.h"
 #include <algorithm>
+#include <sstream>
+//#include <numbers>
 
 namespace vsite::oop::v6
 {
+	bool is_flush(const std::string* cards, int count)
+	{
+		char firstColor = cards[0][0];
 
+		for (int i = 1; i < count; i++)
+		{
+			if (firstColor != cards[i][0])
+				return false;
+		}
+		return true;
+	}
+
+	std::string numpy::name() const
+	{
+		return "NumPy";
+	}
+
+	std::string numpy::version() const
+	{
+		return "1.23.5";
+	}
+
+	int numpy::year() const
+	{
+		return 2022;
+	}
+
+	rectangle::rectangle(int x1, int y1, int x2, int y2) :
+		x1(x1), y1(y1), x2(x2), y2(y2) { }
+
+	void rectangle::print(std::ostream& ss)
+	{
+		ss << "(" << x1 << "," << y1 << ") - (" << x2 << "," << y2 << ")";
+	}
+
+	rectangle& rectangle::move(int dx, int dy)
+	{
+		x1 += dx;
+		x2 += dx;
+		y1 += dy;
+		y2 += dy;
+
+		return *this;
+	}
+
+	polygon::polygon(int size) :
+		size(size), points(new point[size]) { }
+
+	void polygon::add(const point& newPoint)
+	{
+		points[pointCount] = newPoint;
+		pointCount++;
+	}
+
+	double polygon::perimeter()
+	{
+		double peri = 0;
+		for (int i = 0; i < pointCount; i++)
+		{
+			point a = points[i]; // 0, 1, 2, 3
+			point b = i+1 >= pointCount? points[0] : points[i+1]; // 1, 2, 3, 0
+
+			double current_side = sqrt((b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y));
+			peri += current_side;
+		}
+		return peri;
+	}
 }
--- app/app.h
+++ app/app.h
@@ -2,7 +2,61 @@
 #include <string>
 #include <memory>
 
+namespace py
+{
+	class package
+	{
+	public:
+		virtual ~package() = default;
+		virtual std::string name() const = 0;
+		virtual std::string version() const = 0;
+		virtual int year() const = 0;
+	};
+}
+
 namespace vsite::oop::v6 
 {
+	bool is_flush(const std::string* cards, int count);
 
+	class numpy : public py::package
+	{
+	public:
+		std::string name() const override;
+		std::string version() const override;
+		int year() const override;
+		~numpy() override { };
+	};
+
+	class rectangle
+	{
+	private:
+		int x1, y1, x2, y2;
+	public:
+		rectangle(int x1, int y1, int x2, int y2);
+
+		void print(std::ostream& ss);
+		rectangle& move(int dx, int dy);
+	};
+
+	struct point
+	{
+	public:
+		int x, y;
+
+	};
+
+	class polygon
+	{
+	private:
+		int size;
+		int pointCount = 0;
+
+		point* points;
+
+	public:
+		polygon(int size);
+
+		void add(const point& newPoint);
+		double perimeter();
+	};
 }
