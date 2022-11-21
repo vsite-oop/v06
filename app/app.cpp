f248b1d3e8de8641a550d5d41900e48db929041d
Deni Kiseljak <dekiseljak@vsite.hr>
2022-11-21 20:29:56 +0100

    initial commit, first 2 tests pass

--- app/app.cpp
+++ app/app.cpp
@@ -1,7 +1,41 @@
 #include "app.h"
 #include <algorithm>
+#include <format>
+#include <sstream>
+
 
 namespace vsite::oop::v6
 {
+	bool is_flush(const std::string cards[], unsigned num_of_cards) {
+		int same_suit = 0;
+		char suit = cards[0][0];
 
+		for (unsigned i = 1; i < num_of_cards; ++i) {
+			if (cards[i][0] == suit && num_of_cards >= 5) {
+				continue;
+			}
+			else {
+				return false;
+			}
+		}
+	}
 }
+
+/*-------------------test 2----------------------------*/
+
+py::package::~package(){};
+
+std::string numpy::name() const { return "NumPy"; }
+std::string numpy::version() const { return "1.23.5"; }
+int numpy::year() const { return 2022; }
+
+
+
+/*--------------------------test 4-----------------------------*/
+
+rectangle::rectangle(int x1, int y1, int x2, int y2) { _x1 = x1, _y1 = y1, _x2 = x2, _y2 = y2; }
+rectangle::~rectangle() {};
+std::string rectangle::print() const {
+
+};
+
--- app/app.h
+++ app/app.h
@@ -5,4 +5,40 @@
 namespace vsite::oop::v6 
 {
 
+	bool is_flush(const std::string cards[], unsigned num_of_cards);
+
 }
+
+
+/*------------------------test 2----------------------*/
+namespace py
+{
+	class package {
+	public:
+		virtual std::string name() const = 0;
+		virtual std::string version() const = 0;
+		virtual int year() const = 0;
+		virtual ~package() = default;
+	};
+
+	
+}
+
+class numpy :public py::package {
+	std::string name() const override;
+	virtual std::string version() const override;
+	virtual int year() const override;
+	
+};
+
+/*----------------------------test 3-------------------------------*/
+class rectangle {
+	int _x1, _y1, _x2, _y2;
+public:
+	rectangle(int x1, int y1, int x2, int y2);
+	virtual ~rectangle();
+	virtual std::string print() const;
+	void move(int x, int y);
+};
+
+
