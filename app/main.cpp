ddf587aed45fbdefda8cc82196ef5e26545f8671
louis20th <lujomirs@gmail.com>
2023-01-29 17:30:00 +0100

    Added main()

--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,23 @@
 #include "app.h"
+#include <iostream>
 
+using namespace vsite::oop::v6;
 int main()
 {
+    std::cout << "**************** Perimeter calculator *****************" << std::endl;
+    std::cout << "Insert number of vertices: ";
+    int vertices;
+    std::cin >> vertices;
+    auto poli = polygon(vertices);
+
+    for (int i = 0; i < vertices; ++i) {
+        int x, y;
+        std::cout << "Insert X coordinate for point " << i + 1 << " :";
+        std::cin >> x;
+        std::cout << "Insert Y coordinate for point " << i + 1 << " :";
+        std::cin >> y;
+        poli.add({ x, y });
+    }
+
+    std::cout << "Perimeter is: " << poli.perimeter() << std::endl;
 }
