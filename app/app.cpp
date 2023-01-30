5ea2229b176d626e3725f7f0640955c4d98fc87a
Ivan Selendic <ivan.selendic@gmail.com>
2023-01-30 11:20:02 +0100

    Tests passed

--- app/app.cpp
+++ app/app.cpp
@@ -1,7 +1,45 @@
 #include "app.h"
 #include <algorithm>
+#include <type_traits>
+#include <sstream>
 
 namespace vsite::oop::v6
 {
+	bool is_flush(const std::string card[], int x) {
+		for (int i = 1; i < x; ++i) {
+			if (card[i][0] != card[0][0])
+				return false;
+		}
+		return true;
+	}
 
+
+	void rectangle::print(std::ostream& ss) const {
+		ss << "(" + std::to_string(x1) + "," + std::to_string(y1) + ") - (" + std::to_string(x2) + "," + std::to_string(y2) + ")";
+	}
+
+
+	rectangle& rectangle::move(int dx, int dy) {
+		x1 += dx; x2 += dx;
+		y1 += dy; y2 += dy;
+		return *this;
+
+
+	}
+	void polygon::add(const point& pt) {
+		p[n++] = pt;
+	}
+
+
+	double  polygon::distance(const point& a, const point& b) {
+		int dx = b.x - a.x, dy = b.y - a.y;
+		return sqrt(dx * dx + dy * dy);
+	}
+
+	double polygon::perimeter() {
+		double d = distance(p[n - 1], p[0]);
+		for (int i = 0; i < n - 1; ++i)
+			d += distance(p[i], p[i + 1]);
+		return d;
+	}
 }
--- app/app.h
+++ app/app.h
@@ -1,8 +1,60 @@
 #pragma once
 #include <string>
 #include <memory>
+#include <type_traits>
+#include<sstream>
 
-namespace vsite::oop::v6 
+namespace vsite::oop::v6
 {
+	bool is_flush(const std::string card[], int x);
+
+
+	namespace py {
+		class package {
+		public:
+			virtual  std::string name() const = 0;
+			virtual  std::string version() const = 0;
+			virtual  int year() const = 0;
+			virtual ~package() = 0 {};
+		};
+
+
+
+	}
+	class numpy : public py::package {
+	public:
+		std::string name() const override { return "NumPy"; }
+		std::string version() const override { return "1.23.5"; }
+		int year() const override { return 2022; }
+
+	};
+
+	class rectangle {
+		int x1, y1, x2, y2;
+	public:
+		rectangle(int x1, int y1, int x2, int y2) : x1(x1), y1(y1), x2(x2), y2(y2) {};
+		void print(std::ostream& ss) const;
+		rectangle& move(int dx, int dy);
+
+	};
+
+
+	struct point { int x, y; };
+
+	class polygon {
+		int n; point* p;
+	public:
+		polygon(int n) :p(new point[n]), n(0) {}
+		polygon(const polygon& other) : p(new point[other.n]), n(other.n) {
+			for (int i = 0; i < n; ++i)
+				p[i] = other.p[i];
+		}
+		~polygon() { delete[] p; }
+
+		void add(const point& pt);
+		double distance(const point& a, const point& b);
+		double perimeter();
+	};
+
 
 }
