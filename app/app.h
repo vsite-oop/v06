86cf8a2cc24173e813dd797d21674a1e4768dce5
Sonja Perković <soperkov@vsite.hr>
2023-12-04 19:50:04 +0100

    Sperkovic Vježba 6

--- app/app.h
+++ app/app.h
@@ -1,6 +1,28 @@
 #pragma once
+#include <string>
 
 namespace vsite::oop::v6 
 {
+	template <typename T>
+	T minimum(T a, T b) {
+		return a < b ? a : b;
+	}
 
+
+
+	template <typename T, int S>
+	class fixed_array {
+		T arr[S];
+	public:
+		unsigned long long size() {
+			return S;
+		}
+		T operator[](int i) {
+			return arr[i];
+		}
+	};
+
+
+	template <typename T>
+	struct point{};
 }
