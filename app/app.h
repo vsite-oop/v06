beea6950d0a6d7a84d67bb90fbb5ce60c531466e
Bogjenero <bornagjenero1@gmail.com>
2023-12-04 20:23:41 +0100

    Rijeseni zadaci

--- app/app.h
+++ app/app.h
@@ -3,4 +3,24 @@
 namespace vsite::oop::v6 
 {
 
+	template<typename T>
+	T minimum(T broj1, T broj2)
+	{
+		if (broj1 > broj2)
+			return broj2;
+		else
+			return broj1;
+	}
+	template<typename T ,size_t N>
+	class fixed_array {
+		T arr[N];
+	public:
+		size_t size() { return N; }
+		T operator [](int broj)
+		{
+			return arr[broj];
+		}
+	};
+	template<typename T>
+	struct point {};
 }
