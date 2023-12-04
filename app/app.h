45b82b8abf964e5dbeae41cb939a9d73f32ea3e2
DrHrast <petar.huljek@gmail.com>
2023-12-04 19:50:11 +0100

    P_huljek
    vježba 6.

--- app/app.h
+++ app/app.h
@@ -1,6 +1,31 @@
 #pragma once
-
+#include <string>
+//workflow 
 namespace vsite::oop::v6 
 {
+	template<typename p>
+	p minimum(p a, p b) {
+		return a < b ? a : b;
+	}
 
+
+	template<typename T, int S>
+	class fixed_array {
+	private:
+		T arr[S];
+
+	public:
+		unsigned long long size() {
+			return S;
+		}
+
+		T operator[] (int a) {
+			return arr[a];
+		}
+	};
+
+	template<typename T>
+	struct point{
+		T a;
+	};
 }
