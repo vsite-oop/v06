1acd7add79cec1a11717cff1be181c4b801498f9
Marko Pavić <mapavic@vsite.hr>
2023-12-04 20:21:51 +0100

    Marko Pavic

--- app/app.h
+++ app/app.h
@@ -2,5 +2,21 @@
 
 namespace vsite::oop::v6 
 {
+	template<typename Type>
+	Type minimum(Type a, Type b) {
+		return (a < b) ? a : b;
+	}
 
+	template<typename T, int N>
+	class fixed_array {
+		T arr[N];
+	public:
+		size_t size() { return N; }
+		T& operator[](int i) { return arr[i];}
+	};
+
+	template<typename T>
+	struct point {
+		T x, y;
+	};
 }
