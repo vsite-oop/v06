84329435854227b030b19360991e35bb0284209c
Mislav Ćavarušić <micavaru@vsite.hr>
2023-12-04 19:42:17 +0100

    Resolve minimum and fixed array templates

--- app/app.h
+++ app/app.h
@@ -1,6 +1,56 @@
 #pragma once
 
+#include <cstddef>
+#include <algorithm>
+
 namespace vsite::oop::v6 
 {
+    template <typename T>
+    struct point {
+        T x;
+        T y;
+    };
 
+    template <typename T>
+    const T& minimum(const T& a, const T& b) {
+        return (a < b) ? a : b;
+    }
+
+    template <typename T, std::size_t Size>
+    class fixed_array {
+    public:
+        using value_type = T;
+
+        fixed_array() = default;
+
+        const T& operator[](std::size_t index) const {
+            return data[index];
+        }
+
+        std::size_t size() const {
+            return Size;
+        }
+
+    private:
+        T data[Size];
+    };
+
+    template <typename T>
+    class fixed_array<point<T>, 30> {
+    public:
+        using value_type = point<T>;
+
+        fixed_array() = default;
+
+        const point<T>& operator[](std::size_t index) const {
+            return data[index];
+        }
+
+        std::size_t size() const {
+            return 30;
+        }
+
+    private:
+        point<T> data[30];
+    };
 }
--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,30 @@
 #include "app.h"
+#include <iostream>
 
 int main()
 {
+    using namespace vsite::oop::v6;
+    using namespace std::string_literals;
+
+    std::cout << "Testing minimum function:" << std::endl;
+    std::cout << "minimum(12, 13): " << minimum(12, 13) << std::endl;
+    std::cout << "minimum(12u, 13u): " << minimum(12u, 13u) << std::endl;
+    std::cout << "minimum(-10, -5): " << minimum(-10, -5) << std::endl;
+    std::cout << "minimum(3.5, 4.6): " << minimum(3.5, 4.6) << std::endl;
+    std::cout << "minimum(-1.23f, -4.56f): " << minimum(-1.23f, -4.56f) << std::endl;
+    std::cout << "minimum(\"abc\", \"de\"): " << minimum("abc"s, "de"s) << std::endl;
+    std::cout << "minimum(\"DEF\", \"ab\"): " << minimum("DEF"s, "ab"s) << std::endl;
+
+    std::cout << "\nTesting fixed_array template:" << std::endl;
+
+    fixed_array<int, 10> intArray;
+    std::cout << "intArray.size(): " << intArray.size() << std::endl;
+
+    fixed_array<double, 20> doubleArray;
+    std::cout << "doubleArray.size(): " << doubleArray.size() << std::endl;
+
+    fixed_array<point<double>, 30> pointArray;
+    std::cout << "pointArray.size(): " << pointArray.size() << std::endl;
+
+    return 0;
 }
