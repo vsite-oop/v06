ea1c28393b81f21ba79274f4155e724e621b0228
Mihael Krpes <mikrpes@vsite.hr>
2023-12-04 21:04:24 +0100

    v06

--- app/app.h
+++ app/app.h
@@ -2,5 +2,19 @@
 
 namespace vsite::oop::v6 
 {
+	template<typename T>
+	T minimum(T a, T b) {
+		return (a > b) ? b : a;
+	}
 
+	template<typename T, unsigned long long n>
+	class fixed_array {
+		T arr[n];
+	public:
+		unsigned long long size() { return n; }
+		T& operator[](int i) { return arr[i]; }
+	};
+
+	template<typename T>
+	struct point{};
 }
