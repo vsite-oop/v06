7b8083232b37f9dabd5104ad062bd223fa8e92e8
Đuro Rukavina <djrukavina@vsite.hr>
2023-12-04 19:48:56 +0100

    Resolved template

--- app/app.h
+++ app/app.h
@@ -2,5 +2,21 @@
 
 namespace vsite::oop::v6 
 {
+	template<typename T>
+	T minimum(T a, T b) {
+		return (a < b) ? a : b;
+	}
 
+	template<typename T, int N>
+	class fixed_array {
+		T arr[N];
+	public:
+		size_t size() { return N; }
+		T operator[] (int i) { return arr[i]; }
+	};
+
+	template<typename T>
+	struct point {
+		T p;
+	};
 }
