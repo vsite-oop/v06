0b39b9f1caf9f6022c3726d57b1e1cb3601f1307
AnMal <junkie_@msn.com>
2023-12-05 07:10:21 +0100

    AMaletic

--- app/app.h
+++ app/app.h
@@ -2,5 +2,27 @@
 
 namespace vsite::oop::v6 
 {
+	template <typename T>
+	T minimum(T a, T b) {
+		return a < b ? a : b;
+	}
 
+	template <typename T, int SIZE>
+	class fixed_array {
+		T arr[SIZE];
+	public:
+		unsigned long long size() const {
+			return SIZE;
+		}
+
+		T operator [] (int i) {
+			return arr[i];
+		}
+	};
+
+	template <typename T>
+	struct point {
+		T x;
+		T y;
+	};
 }
