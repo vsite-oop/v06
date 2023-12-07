539e8590d72b3f857caf8ffc9097f343c3940a06
Luka Blagojević <lublagoj@vsite.hr>
2023-12-07 18:09:48 +0100

    vjezba6

--- app/app.h
+++ app/app.h
@@ -1,6 +1,28 @@
 #pragma once
 
-namespace vsite::oop::v6 
+namespace vsite::oop::v6
 {
+	template <typename T>
+	T minimum(T a, T b) {
+		return a < b ? a : b;
+	}
 
+	template <typename T, int SIZE>
+	class fixed_array {
+		T arr[SIZE];
+	public:
+		unsigned long long size() const {
+			return SIZE;
+		}
+
+		T operator [] (int i) {
+			return arr[i];
+		}
+	};
+
+	template <typename T>
+	struct point {
+		T x;
+		T y;
+	};
 }
