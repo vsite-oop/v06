0c3e176ff72fd72ac58244095b7de8bc7cf3b34e
Josip Grabušić <jograbus@vsite.hr>
2023-12-10 16:36:56 +0100

    Vježba 6 - prolaze testovi

--- app/app.h
+++ app/app.h
@@ -2,5 +2,34 @@
 
 namespace vsite::oop::v6 
 {
+	template <typename broj>
+	broj minimum(broj x, broj y) 
+	{
+		return x < y ? x : y;
+	}
+
+	template <typename broj, int n>
+	class fixed_array
+	{
+		broj arr[n];
+
+	public:
+		unsigned long long size()
+		{
+			return n;
+		}
+
+		broj operator[] (int i)
+		{
+			return arr[i];
+		}
+	};
+
+	template<typename broj>
+	struct point 
+	{
+		broj x, y;
+	};
+
 
 }
