d5a1b15a36e9af723166eb297bb8365f7a8962d5
Tin Bašić <tin.basic22@gmail.com>
2023-12-11 12:25:51 +0100

    Tin Bašić - V06

--- app/app.h
+++ app/app.h
@@ -2,5 +2,32 @@
 
 namespace vsite::oop::v6 
 {
+	template <typename T>
+	T minimum(T a, T b) 
+	{
+		if (a < b) return a;
+		else
+			return b;
 
+	}
+
+
+	template <typename T, int SIZE>
+	class fixed_array {
+		T arr[SIZE];
+	public:
+		size_t size() const {
+			return SIZE;
+		}
+
+		T& operator [] (size_t i) {
+			return arr[i];
+		}
+	};
+
+	template <typename T>
+	struct point {
+		T x;
+		T y;
+	};
 }
